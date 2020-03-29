cb3753d79654d04a70022cdc734aff2b617348f1
weilunhuang@parto <weilunhuang1@gmail.com>
2020-03-28 20:56:12 -0400

    rename and add index files

--- run_test_fps.sh
+++ run_test_fps.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-#echo "fps=1"
-#python eval.py  --file_list list.txt --data_dir PartoImg/test_fps/pcb_fps_1
+echo "fps=1"
+python eval.py  --file_list list.txt --data_dir PartoImg/test_fps_1920x1080/pcb_fps_1
 
 echo "fps=2"
-python eval.py  --file_list list.txt --data_dir PartoImg/test_fps/pcb_fps_2 --upscale_factor 2 --model weights/RBPN_2x.pth
-
-echo "fps=4"
-python eval.py  --file_list list.txt --data_dir PartoImg/test_fps/pcb_fps_4  --upscale_factor 2 --model weights/RBPN_2x.pth
+python eval.py  --file_list list.txt --data_dir PartoImg/test_fps_1920x1080/pcb_fps_2
 
 echo "fps=10"
-python eval.py  --file_list list.txt --data_dir PartoImg/test_fps/pcb_fps_10 --upscale_factor 2 --model weights/RBPN_2x.pth
+python eval.py  --file_list list.txt --data_dir PartoImg/test_fps_1920x1080/pcb_fps_10
 
 echo "fps=30"
-python eval.py  --file_list list.txt --data_dir PartoImg/test_fps/pcb_fps_30 --upscale_factor 2 --model weights/RBPN_2x.pth
+python eval.py  --file_list list.txt --data_dir PartoImg/test_fps_1920x1080/pcb_fps_30
 
-echo "fps=60"
-python eval.py  --file_list list.txt --data_dir PartoImg/test_fps/pcb_fps_60 --upscale_factor 2 --model weights/RBPN_2x.pth
+# echo "fps=60"
+# python eval.py  --file_list list.txt --data_dir PartoImg/test_fps/pcb_fps_60 --upscale_factor 2 --model weights/RBPN_2x.pth
